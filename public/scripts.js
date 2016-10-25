4ad909bf02a5e71b7f6b1d17356692c2a5179218
Eva Lehtinen <evamlehtinen@gmail.com>
2016-10-25 12:57:05 +0300

    getting imgur embeds with image id. cant get image url since image url is differend than image id

--- public/scripts.js
+++ public/scripts.js
@@ -2,11 +2,16 @@
 $(document).ready(function(){
 
   $('#submitbtn').click(function() {
-      var textsearch = $('input').val();
-      $('.image').text(textsearch);      
-        $.get( 'images', {search: textsearch}, function(data) {
-          $('.image').text(data);      
-          
+      var textsearch = $('input').val(); 
+      $('.images').empty();
+        $.get( 'images', {search: textsearch}, function(data) {            
+            $.each(data, function(i, item) {
+                $('.images').append('<div><blockquote class="imgur-embed-pub" lang="en" data-id="a/'+data[i].id+'"><a href="//imgur.com/'+data[i].id+'">'+data[i].description+'</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script></div>'); 
+                console.log(data[i].id);
+            })
+              
         });
   });
 });
+
+//<blockquote class="imgur-embed-pub" lang="en" data-id="2GuAESk"><a href="//imgur.com/2GuAESk">My face when I do my taxes and figure out how little I made as an illustrator this year... O.C.</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
